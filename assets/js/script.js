6ac736b9044d01ec197553eb28e6c3e2bbd79a75
Sujatagoyat <sujatagoyat8@gmail.com>
2024-03-12 12:06:18 +0530

    added missing ellipse,updated code errors and cross icon

--- assets/js/script.js
+++ assets/js/script.js
@@ -114,3 +114,18 @@ $(document).ready(function () {
     })
 });
 historytabs('tabs1')
+
+// ============ back to top btn =============================
+const topBtn = document.querySelector(".topBtn");
+const show = document.querySelector(".show");
+
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 60) {
+        topBtn.classList.add("show");
+    } else {
+        topBtn.classList.remove("show");
+    }
+})
+topBtn.addEventListener("click", function () {
+    document.documentElement.scrollTop = 0;
+})
